628588e3b0c6e716ccf41be06d9c588de95b8d8c
DartPelmen <kyzaiv1@yandex.ru>
2020-11-16 09:42:51 +1000

    requirements.txt

--- backend/skud/skud/settings.py
+++ backend/skud/skud/settings.py
@@ -10,9 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -127,3 +131,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
